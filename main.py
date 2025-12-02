c0f522a0901732258252928dcfe90330079794be
Matin Fekri <fekridotwork@gmail.com>
2025-12-02 16:32:58 +0330

    feat(main):implement a basic user-panel

---
+++ main.py
@@ -0,0 +1,32 @@
+def main():
+    while True:
+        print("\n--- Reminder Manager ---")
+        print("1) Add reminder")
+        print("2) Remove reminder")
+        print("3) List reminders")
+        print("4) Execute all reminders")
+        print("5) Reminder grouping")
+        print("6) Finding a reminder by id")
+        print("7) Exit")
+        choice = input("Choose an option: ").strip()
+
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "6":
+            pass
+        elif choice == "7":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid option --> Try again")
+
+if __name__ == "__main__":
+    main()
