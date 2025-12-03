63d02460ae92fc37b46706e58c0f56b9c6e5e738
Matin Fekri <fekridotwork@gmail.com>
2025-12-03 10:46:26 +0330

    feat(Reminder) : class implementation

---
+++ models/Reminder.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+@dataclass
+class Reminder(ABC):
+    title: str
+    time: str
+    rem_id: int
+
+    @abstractmethod
+    def remind(self) -> None:
+        #Polymorphic implementation
+        raise NotImplementedError
